33bd8be244463bf1dbdcbbc66f00a6cba6973d0e
Allan Imire <imireallan@gmail.com>
2020-06-02 15:38:21 +0300

    change header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,15 +4,8 @@ import '../css/Header.css'
 function Header() {
 	return (
         <header className="header">
-            <div className="container flex">
+            <div className="container center">
                 <a className="header__logo" href="#">Chuck Norris<sup>JOKES</sup></a>
-                <nav className="nav">
-                    <ul className="nav__list flex">
-                        <li className="nav__list__item"><a className="nav__link" href="#">home</a></li>
-                        <li className="nav__list__item"><a className="nav__link" href="#">about</a></li>
-                        <li className="nav__list__item"><a className="nav__link" href="#">contact</a></li>
-                    </ul>
-                </nav>
             </div>
         </header>
     );
